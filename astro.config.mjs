7932b660cc99d20bcd5fb911760171911d1f0a16
NL54 <92691452+NL54@users.noreply.github.com>
2023-06-05 11:14:07 +0200

    For netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://NL54.github.io',
-	base: '/portfolio',});
+  site: 'https://NL54.github.io',
+  base: '/portfolio',
+  output: "server",
+  adapter: netlify()
+});
